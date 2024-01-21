0a7911a13dad1576795f70e022e7c934829fbd9d
Magal <Magal@LAPTOP-73LE3T5F>
2024-01-21 14:49:23 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,114 @@
+﻿// single-line comment or
+/* multi-line comment */
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1.   I played soccer in HighSchool");
+            Console.WriteLine("2.   I was in Academic Decathalon in HighSchool");
+            Console.WriteLine("3.   I played tennis in HighSchool");
+            Console.WriteLine("4.   I ran track in HighSchool");
+            Console.WriteLine("5.   I'm 5'11");
+            Console.WriteLine("6.   I'm Mexican");
+            Console.WriteLine("7.   I live in California");
+            Console.WriteLine("8.   I enjoy all kinds of music");
+            Console.WriteLine("9.   I was in the U.S. Marines");
+            Console.WriteLine("10.  I worked as an aircraft firefighter");
+            Console.WriteLine("11.  I worked as a wildland firefighter");
+            Console.WriteLine("12.  I worked as a structural firefighter");
+            Console.WriteLine("13.  I have certificatiosn in HAZMAT");
+            Console.WriteLine("14.  I like to box");
+            Console.WriteLine("15.  I love to explore nature");
+            Console.WriteLine("16.  I'm a terrible swimmer");
+            Console.WriteLine("17.  I have certifications in National incident management");
+            Console.WriteLine("18.  I have drifted a government vehicle");
+            Console.WriteLine("19.  I enjoy reading");
+            Console.WriteLine("20.  I love the sound of violins playing in harmony");
+            Console.WriteLine("21.  I first used a computer in 2005");
+            Console.WriteLine("22.  I'm currently 24 years old");
+            Console.WriteLine("23   I can Deadlift 500 lbs");
+            Console.WriteLine("24.  I can squat 315 lbs");
+            Console.WriteLine("25.  I can bench 225 lbs");
+            Console.WriteLine("26.  I bought my first vehicle at 17");
+            Console.WriteLine("27.  I have a custom axe");
+            Console.WriteLine("28.  I like working on cars");
+            Console.WriteLine("29.  I play Apex");
+            Console.WriteLine("30.  I first learned about coding thanks to minecraft");
+            Console.WriteLine("31.  My biggest fear is loneliness");
+            Console.WriteLine("32.  My biggest pet peeve is people not closing a door");
+            Console.WriteLine("33.  My two idols are David Goggins and J.Cole");
+            Console.WriteLine("34.  I wear glasses");
+            Console.WriteLine("35.  I have one tattoo");
+            Console.WriteLine("36.  I have 7 scars");
+            Console.WriteLine("37   My favorite movie genre is comedy");
+            Console.WriteLine("38.  When I was a child I wanted to be a firefighter");
+            Console.WriteLine("39.  I'm working on learning a third language");
+            Console.WriteLine("40.  I like watching UFC");
+            Console.WriteLine("41.  My go to karaoke song is Yo Soy El Aventurero");
+            Console.WriteLine("42.  My childhood nickname was yogi");
+            Console.WriteLine("43.  My military nickname was OG");
+            Console.WriteLine("44.  My favorit animals are tigers");
+            Console.WriteLine("45.  My favorite color is sky blue");
+            Console.WriteLine("46.  My favorite book is into the wild");
+            Console.WriteLine("47.  My favorite food is my moms cooking");
+            Console.WriteLine("48.  I like to cook");
+            Console.WriteLine("49.  I like to bake");
+            Console.WriteLine("50.  I like to grill");
+            Console.WriteLine("51.  I speak Spanish mexican and spaniard variation");
+            Console.WriteLine("52.  I speak English");
+            Console.WriteLine("53.  I'm Right handed");
+            Console.WriteLine("54.  My favorite holiday is Christmas");
+            Console.WriteLine("55.  My go to fast food is Chik-fil-a");
+            Console.WriteLine("56.  My life goal is to begin generational wealth for my family");
+            Console.WriteLine("57.  chicken Katsu curry is my favorite dish");
+            Console.WriteLine("58.  I'm not the biggest fan of roller coasters");
+            Console.WriteLine("59.  My favorite movie is Rush Hour 2");
+            Console.WriteLine("60.  Sociology is my favorite subject");
+            Console.WriteLine("61.  I've ate crickets before");
+            Console.WriteLine("62.  I collect dollar coins");
+            Console.WriteLine("63.  I'm an extroverted person");
+            Console.WriteLine("64.  I like to learn about mechanics");
+            Console.WriteLine("65.  I have a bad habit of zoning out");
+            Console.WriteLine("66.  I perfer pancakes over waffles");
+            Console.WriteLine("67.  I love dark humor");
+            Console.WriteLine("68   I will never try blue cheese again");
+            Console.WriteLine("69.  I have very good peripheral vision");
+            Console.WriteLine("70.  I'm a taurus");
+            Console.WriteLine("71.  I like star gazing");
+            Console.WriteLine("72.  I like reading self development books");
+            Console.WriteLine("73.  My favorite song is let nas down");
+            Console.WriteLine("74.  I have a dog");
+            Console.WriteLine("75.  I'm the oldest child in my family");
+            Console.WriteLine("76.  This is my second coding class");
+            Console.WriteLine("77.  I perfer tea over coffee");
+            Console.WriteLine("78.  I love back workouts");
+            Console.WriteLine("79.  My favorite app currently is Tik-Tok");
+            Console.WriteLine("80.  I believe in ghosts");
+            Console.WriteLine("81.  I perfer loyalty over love");
+            Console.WriteLine("82.  My least favorit emotion is despair");
+            Console.WriteLine("83.  Pepporini pizza is my biggest weakness");
+            Console.WriteLine("84.  My favorite memory is a large family gathering that happened back in 2008");
+            Console.WriteLine("85.  I'm a big fan of dad jokes");
+            Console.WriteLine("86.  My favorite ice cream is a mexican flavored called burnt milk");
+            Console.WriteLine("87.  I can dissasemble a rifle with my eyes closed");
+            Console.WriteLine("88.  I like learning about investing");
+            Console.WriteLine("89.  Redbulls are my go to energy drink");
+            Console.WriteLine("90.  My dream car is an FD RX7");
+            Console.WriteLine("91.  I'm going to be the first millionaire in my family");
+            Console.WriteLine("92.  I'm learning how to play acoustic guitar");
+            Console.WriteLine("93.  I know how to sew");
+            Console.WriteLine("94.  I have worked alongside LA County Fire Dept.");
+            Console.WriteLine("95.  I like pushing myself and improving");
+            Console.WriteLine("96.  I have flown in a helicopter");
+            Console.WriteLine("97.  I drive a chevy silverado");
+            Console.WriteLine("98.  I've been in a private jet");
+            Console.WriteLine("99.  I know how to read a map");
+            Console.WriteLine("100. I've raced in a firetruck");
+
+        }
+    }
+}
